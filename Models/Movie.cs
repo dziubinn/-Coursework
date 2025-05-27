921f182950b5c6cfb16a1f165e186bdfdd4dda19
Володимир Дзюбін <volodymyr.dziubin@nure.ua>
2025-05-27 20:41:58 +0300

    +Додано клас Film

---
+++ Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сoursework.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Studio { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public string Country { get; set; }
+        public string Director { get; set; }
+        public string MainActors { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+        public int Duration { get; set; }
+        public bool Watched { get; set; }
+        public bool IsFavorite { get; set; }
+
+    }
+}
